da80741518aa51fc4a57f3e5b5897201f587d4fd
Luís Arteiro <lucho14garfield@gmail.com>
2024-06-21 15:39:25 +0400

    chore: Deleting test-results folder that shouldn't be versioned in the first place.

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,5 +1,12 @@
-// must use this fixture instead of "@playwright/test" to be able to collect data for coverage
-import { test, expect } from "../_shared/app-fixtures";
+// !! PLEASE READ !!
+/* 
+Coverage for E2E effectively works, but the coverage is not using the sourcemaps properly, so coverage doesn't mean much.
+We've decided to leave the infrastructure (coverage uses `monocart`) with its configuration for posteriority in case we need them.
+E2E testing is more relevant knowing **if they pass or not**.
+*/
+
+
+import { test, expect } from "../_shared/app-fixtures"; // we use the overriden functions to get coverage
 
 test.describe("homepage", () => {
   test("is mounted", async ({ page }) => {

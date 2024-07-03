34a6327e00b941b50dffdbbed99ab6bf294511a4
Luís Arteiro <lucho14garfield@gmail.com>
2024-07-03 16:23:10 +0100

    chore: Using .tsx config theme

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withNextra = require("nextra")({
   theme: "./theme/src/index.tsx",
-  themeConfig: "./theme.config.jsx",
+  themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true
 });
 

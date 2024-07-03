680443fb7fc3fa0e752f5efaf2dc2d225898f7fc
Luís Arteiro <lucho14garfield@gmail.com>
2024-07-03 16:22:55 +0100

    chore: Converting theme config to .tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,10 @@
-export default {
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
     logo: <span>My Nextra Documentation</span>,
     project: {
       link: 'https://github.com/shuding/nextra'
     }
   }
+
+  export default config

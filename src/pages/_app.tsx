5f258b20b71f7b2dd92797feffde81ec0e21f47e
Luís Arteiro <lucho14garfield@gmail.com>
2024-07-08 10:00:52 +0100

    chore: Removing erronous ; symbol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function MyApp({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </SessionProvider>
   )
 }

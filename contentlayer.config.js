6c3f2968c85a632fce089b4db190bad03efbab00
Luís Arteiro <lucho14garfield@gmail.com>
2024-07-25 23:59:35 +0100

    feat: Adding linting all links.

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,23 +1,26 @@
 import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
 
-const computedFields = {
+const computedFieldsPage = {
   slug: {
     type: 'string',
-    resolve: (doc) => doc._raw.sourceFileName.replace(/\.md$/, ''),
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
   },
 }
 
 export const Page = defineDocumentType(() => ({
   name: 'Page',
   filePathPattern: `**/*.mdx`,
-  computedFields,
+  computedFieldsPage,
 }))
 
 export const MetaJson = defineDocumentType(() => ({
   name: 'MetaJson',
   filePathPattern: `**/_meta.json`,
 }))
+
 export default makeSource({
   contentDirPath: 'src/pages',
-  documentTypes: [Page, MetaJson],
+  documentTypes: [Page],
+  onUnknownDocuments: 'skip-ignore'
+
 })
--- scripts/link-check.mjs
+++ scripts/link-check.mjs
@@ -2,37 +2,46 @@ import markdownLinkCheck from "markdown-link-check";
 import { allDocuments } from "../.contentlayer/generated/index.mjs";
 
 const baseUrl = process.argv.find((arg) => arg.includes("--baseUrl"));
-const allBody = allDocuments.map(({ body }) => body.raw).join("\n--- NEXT ARTICLE ---\n");
+const allBody = allDocuments.map(({ body }) => body.raw).join("\n--- NEXT PAGE ---\n");
 
+// Options for `markdown-lint-check`
 const configOpts = {
   projectBaseUrl: baseUrl ? baseUrl.replace("--baseUrl=", "https://") : "http://localhost:3000",
   replacementPatterns: [
+    // Match links like `[example](/example)
     {
       pattern: "^/",
+      replacement: "{{BASEURL}}/",
+    },
+    // Match links like `[example](./example)
+    {
+      pattern: "^./",
       replacement: "{{BASEURL}}/",
     },
   ],
 };
 
+// NOTE:
+// Runs `markdown-link-check`
+// If this step fails, check the output. You should find "input". It shows the link that caused the error.
+// Most likely the reason it's failing is because the link doesn't start with `./` or `/` (e.g. [example](index.md), instead of [example](./index.md)).
 markdownLinkCheck(allBody, configOpts, function (error, linkCheckresults) {
   try {
     if (error) throw new Error(error);
 
     const results = linkCheckresults.map((linkCheckResult) => ({ ...linkCheckResult }));
-    console.log(results)
     const hasDeadLink = results.find((result) => result.status.includes("dead"));
 
-    results.forEach(({ link, status}) => {
+    results.forEach(({ link, status }) => {
       console.log("%s is %s", link, status === "dead" ? "dead 💀" : "alive 🥳");
     });
 
     if (hasDeadLink) {
       throw new Error("Dead link found.👆💀");
     } else {
-      console.log("All links are valid! 🙌")
+      console.log("All links are valid! 🙌");
     }
-
-  } catch(error) {
-    throw Error(error.message)
+  } catch (error) {
+    console.error(error.message);
   }
 });

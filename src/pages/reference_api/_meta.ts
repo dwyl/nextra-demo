8228c5d7aa1417f5cdfe6b8af693359690fcaee7
Luís Arteiro <lucho14garfield@gmail.com>
2024-10-14 03:19:34 +0100

    Reapply "deps: Migrate Nextra core to v3."
    This reverts commit 7f1a638d69940d84eb834ac48b4b12a98d51571d.

---
+++ src/pages/reference_api/_meta.ts
@@ -0,0 +1,14 @@
+export default {
+  about: "about",
+  "---": {
+    type: "separator"
+  },
+  person: "person",
+  mega_private: {
+    title: "Mega Private Section",
+    private: {
+      private: true,
+      roles: ["cant_enter"]
+    }
+  }
+};

74770673e0038d0dcbf13ea7a0309195fd665102
Luís Arteiro <lucho14garfield@gmail.com>
2024-10-14 03:19:53 +0100

    Reapply "fix: Fixing errors and deps."
    This reverts commit 827c87a0216304ecddf7aa60cf45ae05c1110520.

--- theme/src/constants.tsx
+++ theme/src/constants.tsx
@@ -181,3 +181,9 @@ export const DEEP_OBJECT_KEYS = Object.entries(DEFAULT_THEME)
     }
   })
   .filter(Boolean)
+
+  export const MARKDOWN_EXTENSION_REGEX = /\.mdx?$/
+
+export const NEXTRA_INTERNAL = Symbol.for('__nextra_internal__')
+
+export const ERROR_ROUTES = new Set(['/404', '/500'])
